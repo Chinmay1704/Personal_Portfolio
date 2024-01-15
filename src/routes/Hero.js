bca873a3720b5e936effe8911f4c41d96b7a1462
Chinmay1704 <cpkhanapurkar1704@gmail.com>
2024-01-15 17:00:10 +0530

    final

--- src/routes/Hero.js
+++ src/routes/Hero.js
@@ -4,17 +4,18 @@ import AboutPage from "../components/AboutPage/Aboutpage";
 import Skillspage from "../components/SkillsPage/Skillspage";
 import ProjectsPage from "../components/ProjectPage/ProjectPage";
 import Contact from "../components/ContactPage/contact";
+import { Link } from "react-router-dom";
 
 
 function Hero() {
   return (
-    <>
+    <Link to={"/"}>
     <HomePage/>
     <AboutPage/>
     <Skillspage/>
     <ProjectsPage/>
     <Contact/>
-    </>
+    </Link>
   );
 }
 
